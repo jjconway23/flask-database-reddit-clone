9fb107572f2f11e49d4c7093831ad41cecb6a476
Jacob Peat <jacob.peat01@gmail.com>
2022-08-05 15:26:52 +0100

    test app opens in browser

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-print("Hello World !")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello world!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
